26171fe18b561d51c532228a884f0f7be26f1a1c
Md. Khalilur Rahman <khalilur.rahman@bjitgroup.com>
2022-03-06 15:04:24 +0600

    Login in progress

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,6 +5,10 @@ import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import Typography from '@material-ui/core/Typography'
+//import { getStorage, ref } from "firebase/storage";
+//const storage = getStorage();
+// Create a child reference
+
 
 const styles = {
     card :{
@@ -23,10 +27,20 @@ const styles = {
 class Product extends Component {
   render() {
       const {classes, product:{thumbImageUrl,title}} = this.props
+     var thumb_img = thumbImageUrl;
+     
+if (thumb_img.charAt(0) == "/") thumb_img = thumb_img.substr(1);
+if (thumb_img.charAt(thumb_img.length - 1) == "/") thumb_img = thumb_img.substr(0, thumb_img.length - 1);
+const thumb_uri = encodeURIComponent(thumb_img)
+
+const imgRef = `https://firebasestorage.googleapis.com/v0/b/bazarano-1b9da.appspot.com/o/${thumb_uri}?alt=media`
+     //const imageRef = "https://storage.googleapis.com/bazarano-1b9da.appspot.com/productPictures/86f71ec6-12e2-35a3-b48b-60c561800ec5?GoogleAccessId=firebase-adminsdk-entqv%40bazarano-1b9da.iam.gserviceaccount.com&Expires=16447017600&Signature=Do%2B7lYoJVd54MADTUfl6CwMEzNrzD86jtn6rS4g01UokGGw4Kq3fYT4IdbYrS3ub%2FkZ%2FDrgvaW1utyTgxOQoEa191JcOUwWAzwZff6WGeEJJ%2FZuOs1fanuKbuvrHzwFiy7hbmKMJZZjAFx9a8zhgInyhjWUjtTrAzmgDyM6eSp6AxdhbN8uuU4IdCPFzeG%2F2D7PSl26dFQtKFKUKnj0zJ4ZphFdGC8mjT4WKySd%2B1H4d72x3H8gcuLtt%2BqAC9YkjpFXgwX9IGh2WbLA%2FPugO%2BWpZDTyBZCfGYXc%2F%2Fiq%2FMSUheSxxRyoVbuS5k62daOT%2Fgn5ZaZpQ3XUk2To6JdEbUQ%3D%3D"
+     
+     console.log(imgRef);
     return (
       <Card className={classes.card}>
           <CardMedia
-          image = "gs://bazarano-1b9da.appspot.com/productPictures/019a5ec4-85fe-3947-8ced-1d35e9674ea2"
+          image = {imgRef}
           title = {title}
           className = {classes.image}
           />
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,89 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+
+
+const styles = {
+  form : {
+    textAlign : 'center'
+  },
+
+pageTitle :{
+  margin : '10px auto 10px auto'
+},
+
+textField : {
+  margin : '10px auto 10px auto'
+},
+
+button : {
+ marginTop : 20
+}
+
+}
 
 class login extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+        email : '',
+        password : '',
+        loading : false,
+        errors : {}
+    }
+  }
+
+  handleSubmit = (event) =>{
+      console.log('hi')
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
+
   render() {
+    const {classes} = this.props
     return (
-      <div>
-         <h1>Login</h1>
-      </div>
+     <Grid container className = {classes.form}>
+        <Grid item sm/>
+        <Grid item sm>
+        <Typography variant="h3" className = {classes.pageTitle}>
+          <p>Login</p>
+        </Typography>
+      
+        <form noValidate onSubmit={this.handleSubmit}>
+      
+        <TextField id='email' name = 'email' type = 'email' label = 'Email' className={classes.textField}
+        value={this.state.email} onChange = {this.handleChange} fullWidth/> 
+        
+         <TextField id='password' name = 'password' type = 'password' label = 'Password' className={classes.textField}
+        value={this.state.password} onChange = {this.handleChange} fullWidth/> 
+         
+         <Button type = 'submit'  variant='contained'
+         color='primary'
+          className = {classes.button}
+        > Login </Button> 
+         
+
+        </form>
+
+        </Grid>
+        <Grid item sm/>
+
+       </Grid>
     )
   }
 }
 
-export default login
+login.propTypes = {
+  classes : PropTypes.object.isRequired
+} 
+export default withStyles(styles)(login)
